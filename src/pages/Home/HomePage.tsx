36893d9eb45311be7c0feb5f8703aedda129d99f
simon <ares.himself@outlook.com>
2025-07-09 23:57:22 +0200

    import global styles, remove unused fonts

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,26 +1,15 @@
-import cube from "@/assets/StartPic.jpg";
-import side from "@/assets/sidePanel.svg";
+
 import styles from "./HomePage.module.scss";
+import cubeImg from "@/assets/CloseUp_Cube_Cycles_Render.jpg"
 
 export default function HomePage() {
   return (
     <>
-      <div className={styles.relative}>
-        <div
-          className={styles.flexAbsolute}
-          style={{ height: "calc(100vh - 3.25rem)" }}
-        >
-          <div className={styles.sidePanel}>
-            <img src={side} />
-          </div>
-          <div className={styles.imageContainer}>
-            <img
-              src={cube}
-              className={styles.cubeImage}
-            />
-            <p className={styles.workText}>WORK from 2025</p>
-          </div>
-        </div>
+      <div className={styles.imageContainer} >
+        <img src={cubeImg} alt="cube image" ></img>
+      </div>
+      <div>
+        
       </div>
     </>
   );
